d39460e1966e0a29fb5d145bf04a720ca884408d
ken <block24block@gmail.com>
2022-05-12 23:24:15 +0800

    get partial stack

--- lib/binding_of_callers.rb
+++ lib/binding_of_callers.rb
@@ -4,28 +4,28 @@ require 'binding_of_caller'
 
 class Binding
 
-  def of_callers
-    enhance do |bi|
+  def of_callers(fc = frame_count)
+    enhance(fc) do |bi|
       BindingOfCallers::Revealed.new bi
     end
   end
 
-  def of_callers!
-    enhance do |bi|
+  def of_callers!(fc = frame_count)
+    enhance(fc) do |bi|
       bi.extend BindingOfCallers::Reveal
     end
   end
 
   private
 
-  def enhance &enhance
-    collected.map(&enhance)
+  def enhance(fc, &enhance)
+    collected(fc).map(&enhance)
   end
 
-  def collected
+  def collected(fc)
     n = 3
     bis = []
-    while n < frame_count
+    while n < fc
       bis << of_caller(n)
       n = n.succ
     end
